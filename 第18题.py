7c81c8c9553c2585dacdff182de12ab3a29d55f9
hzhhzh456 <1277430572@qq.com>
2020-01-31 23:00:24 +0800

    lll

---
+++ 第18题.py
@@ -0,0 +1,4 @@
+import datetime
+print('当年月日:',datetime.data.today())
+print('格式化日期:',datetime.date.today().strftime('%d/%m/%Y'))
+print(datetime.date(2020,1,31))
---
+++ 第33题.py
@@ -0,0 +1,4 @@
+a=10
+for i in range (1,5):
+    a=a+2
+print(a)
